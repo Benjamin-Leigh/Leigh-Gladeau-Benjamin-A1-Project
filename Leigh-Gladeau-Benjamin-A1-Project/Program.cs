a2871c6a0880c8472713f0270af069107b0021e2
Benjamin Leigh-Gladeau <000785704@mohawkcollege.ca>
2024-05-06 10:54:14 -0400

    Add project files.

---
+++ Leigh-Gladeau-Benjamin-A1-Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Leigh_Gladeau_Benjamin_A1_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

f51a9d5e1b63b3cb50a235c948dd1d2a5a57e877
Benjamin Leigh-Gladeau <000785704@mohawkcollege.ca>
2024-05-14 14:45:45 -0400

    Beginning, and asking name.

--- Leigh-Gladeau-Benjamin-A1-Project/Program.cs
+++ Leigh-Gladeau-Benjamin-A1-Project/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Leigh_Gladeau_Benjamin_A1_Project
+﻿using System.Net.Security;
+
+namespace Leigh_Gladeau_Benjamin_A1_Project
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine("You wake up, sun shining through bars within the window.");
+            Console.WriteLine("You sit up and look towards the wooden door with a metal window.");
+            Console.WriteLine("Your mind is foggy, but you can tell you are in a holding cell.");
+            Console.WriteLine("Thinking back to the day before, you and your gang were doing a heist.");
+            Console.WriteLine("Being the lookout for the heist you were in charge of making sure your men know of upcoming issues.\nWhile outside you see the sheriff and his men on horseback over the hill top coming at a fast pace.");
+            Console.WriteLine("You remember going to your men to warn them of incoming law enforcement and they have only a couple minutes to finish.");
+            Console.WriteLine("Taking the initiative you stand by and distract law enforcement, giving your men time to escape with wha they can.");
+            Console.WriteLine("Putting your gun on the ground you try and reason with law enforcement, you hear the movement of horses in the distance.");
+            Console.WriteLine("you remember the sheriffs office, but then after that you woke up.\nYour head is in pain and your shoulder is bruised, must have fell hard.");
+            Console.WriteLine("at that moment you hear movement and then see someones face at the door.");
+            Console.WriteLine("\nMan - Well well well, you got us good there. Had fooled us enough that we lost track of the other members of your gang.");
+            Console.WriteLine("But i dont recognize you, who are you? What is your name?");
+            Console.ReadLine();
+            Console.Write("Hello {}");
         }
     }
 }

819b047f7e24741aa25faa1c8756dd48caf6a4ad
Benjamin Leigh-Gladeau <000785704@mohawkcollege.ca>
2024-05-15 14:39:41 -0400

    WriteLine changes

--- Leigh-Gladeau-Benjamin-A1-Project/Program.cs
+++ Leigh-Gladeau-Benjamin-A1-Project/Program.cs
@@ -6,19 +6,23 @@ namespace Leigh_Gladeau_Benjamin_A1_Project
     {
         static void Main(string[] args)
         {
-            
+
             Console.WriteLine("You wake up, sun shining through bars within the window.");
             Console.WriteLine("You sit up and look towards the wooden door with a metal window.");
             Console.WriteLine("Your mind is foggy, but you can tell you are in a holding cell.");
-            Console.WriteLine("Thinking back to the day before, you and your gang were doing a heist.");
+
+            Console.WriteLine("\nThinking back to the day before, you and your gang were doing a heist.");
             Console.WriteLine("Being the lookout for the heist you were in charge of making sure your men know of upcoming issues.\nWhile outside you see the sheriff and his men on horseback over the hill top coming at a fast pace.");
             Console.WriteLine("You remember going to your men to warn them of incoming law enforcement and they have only a couple minutes to finish.");
             Console.WriteLine("Taking the initiative you stand by and distract law enforcement, giving your men time to escape with wha they can.");
-            Console.WriteLine("Putting your gun on the ground you try and reason with law enforcement, you hear the movement of horses in the distance.");
-            Console.WriteLine("you remember the sheriffs office, but then after that you woke up.\nYour head is in pain and your shoulder is bruised, must have fell hard.");
-            Console.WriteLine("at that moment you hear movement and then see someones face at the door.");
-            Console.WriteLine("\nMan - Well well well, you got us good there. Had fooled us enough that we lost track of the other members of your gang.");
-            Console.WriteLine("But i dont recognize you, who are you? What is your name?");
+            Console.WriteLine("Putting your gun on the ground you try and reason with law enforcement, you hear the movement of horses in the distance.\nNow sure of their depature, you feel better.");
+            Console.WriteLine("You are arrested on sight and are taken to the front of the sheriffs office, but before entering, things go dark.");
+            Console.WriteLine("\nNow remembering how events went and knowing I was knocked out by law enforcement, you realize your situation now.\nDuring that realization you hear footsteps outside the door and moments later see someones face at the door.\nYou recognize him as the sheriff.");
+
+            Console.WriteLine("\nSheriff - \"Well well well, was wondering when you were gonna wake up after that \"Tumble\" you took earlier.");
+            Console.WriteLine("You got us good there, had us fooled just long enough that my deputy lost track of the other members of your gang.");
+            Console.WriteLine("Now you know about me, but i dont recognize you. Who are you? What is your name?\"");
+
             //Getting Readline to remember name, to make it easier to pull it later on.
             string name = Console.ReadLine();
             Console.Write($"Well i wish i could say it's nice to meet you {name}, But we have an issue.");
@@ -29,21 +33,24 @@ namespace Leigh_Gladeau_Benjamin_A1_Project
             Console.WriteLine("\n-Type (Aid) for choice one. - Type (Not Help) for choice two");
             //Separate ReadLine that makes it easier to acknowledge particular choices. 
             string choice = Console.ReadLine();
-            if (choice == "Aid") 
+            if (choice == "Aid")
             {
-                Console.WriteLine($"You made the right choice {name},"); 
+                Console.WriteLine($"You made the right choice {name}, we should get right to business then,\nafterall time is limited.");
                 Console.WriteLine("");
 
                 if (choice == "Not Help")
                 {
-
+                    Console.WriteLine($"well {name}, you are free to that choice.");
+                    Console.WriteLine("");
                 }
             }
             //If you input a choice incorrectly they will assume you dont want either.
             else
             {
-                Console.WriteLine("");
+                Console.WriteLine($"unfortunately for you {name}, no one will know of your disapearance here.\nYou were never heard from again.");
             }
+            Console.WriteLine("");
+
         }
     }
 }

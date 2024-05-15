61223055f2b703cc72cb1172ddbe03de9cf287a1
Benjamin Leigh-Gladeau <000785704@mohawkcollege.ca>
2024-05-15 13:24:40 -0400

    Code comments and starting first choice

--- Leigh-Gladeau-Benjamin-A1-Project/Program.cs
+++ Leigh-Gladeau-Benjamin-A1-Project/Program.cs
@@ -6,6 +6,7 @@ namespace Leigh_Gladeau_Benjamin_A1_Project
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("You wake up, sun shining through bars within the window.");
             Console.WriteLine("You sit up and look towards the wooden door with a metal window.");
             Console.WriteLine("Your mind is foggy, but you can tell you are in a holding cell.");
@@ -18,8 +19,31 @@ namespace Leigh_Gladeau_Benjamin_A1_Project
             Console.WriteLine("at that moment you hear movement and then see someones face at the door.");
             Console.WriteLine("\nMan - Well well well, you got us good there. Had fooled us enough that we lost track of the other members of your gang.");
             Console.WriteLine("But i dont recognize you, who are you? What is your name?");
-            Console.ReadLine();
-            Console.Write("Hello {}");
+            //Getting Readline to remember name, to make it easier to pull it later on.
+            string name = Console.ReadLine();
+            Console.Write($"Well i wish i could say it's nice to meet you {name}, But we have an issue.");
+            Console.WriteLine("\nWe have been following your group for a while now, and it was an annonimous tip\nthat even allowed us to catch you on time.");
+            Console.WriteLine("Im gonna say you have two choices on what you can do right now");
+            Console.WriteLine("First is you can aid us, or you get the law served to you. Choice is yours");
+            //Added something you can see in game so you know exactly how to write a choice.
+            Console.WriteLine("\n-Type (Aid) for choice one. - Type (Not Help) for choice two");
+            //Separate ReadLine that makes it easier to acknowledge particular choices. 
+            string choice = Console.ReadLine();
+            if (choice == "Aid") 
+            {
+                Console.WriteLine($"You made the right choice {name},"); 
+                Console.WriteLine("");
+
+                if (choice == "Not Help")
+                {
+
+                }
+            }
+            //If you input a choice incorrectly they will assume you dont want either.
+            else
+            {
+                Console.WriteLine("");
+            }
         }
     }
 }
